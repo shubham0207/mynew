3cd04695a5a006bfcf41d2b61a520b8285965225
iet <shubhamkankal2795@gmail.com>
2019-12-22 18:34:56 +0545

    hello

---
+++ arrayOperation.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+
+int main()
+{
+printf("enter the size of array");
+int n=0, pos=0,ele=0;
+int a[n];
+
+
+for(int i=0;i<=n;i++)
+{
+scanf("%d",&a[i]);
+}
+
+printf("enter the position you want to insert data");
+scanf("%d",&pos);
+printf("enetr the element");
+scanf("%d",&ele);
+for(i=0;i<=pos;i++)
+{
+
+
+}
+  
+
+
+
+
+return 0;
+}
